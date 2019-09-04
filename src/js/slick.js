fe9f3e26bda7eb10c04423e5aed75982b26c6046
Denis, Makarov <makarovdenis07081985@gmail.com>
2019-09-04 14:33:31 +0300

    first

---
+++ src/js/slick.js
@@ -0,0 +1,12 @@
+//
+// $('.header_slider_box').slick({
+//
+//     infinite: true,
+//     speed: 500,
+//     fade: true,
+//     cssEase: 'linear',
+//     autoplay: true,
+//     autoplaySpeed: 3000,
+//     focusOnSelect: true
+//
+// });
